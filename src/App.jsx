efea976358d81c420f3e0576178a03bacc1dc0d1
Elias Papavlassopoulos <e.papa@live.de>
2022-05-12 14:10:02 +0200

    removed React logo, changed main.jsx render

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container">
     </div>
   );
 }
