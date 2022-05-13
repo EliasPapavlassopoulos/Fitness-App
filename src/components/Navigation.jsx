b4b28832901d55a42608bb9b5514419bbbfcbcd9
Elias Papavlassopoulos <e.papa@live.de>
2022-05-13 15:00:03 +0200

    adds 3 new svgs: active Images for navigation.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,10 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // Home is active on startup
 function Navigation() {
     return (
-        <div className="bg-navigation bg-opacity-40 rounded-t-xs flex justify-around w-full h-12 p-3 items-center fixed bottom-0">
+        <nav className="bg-navigation bg-opacity-40 rounded-t-xs flex justify-around w-full h-12 p-3 items-center fixed bottom-0">
             <NavLink to="/">
-                <img src="../images/navigation-home.svg" />
+                <img src="../images/navigation-home-active.svg" />
             </NavLink>
             <NavLink to="/browse">
                 <img src="../images/navigation-browse.svg" />
@@ -12,7 +12,7 @@ function Navigation() {
             <NavLink to="/profile">
                 <img src="../images/navigation-profile.svg" />
             </NavLink>
-        </div>
+        </nav>
     );
 }
 
