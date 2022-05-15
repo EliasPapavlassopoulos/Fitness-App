f784243a19f4f8302357732b1192a1b5872eb6be
Elias Papavlassopoulos <e.papa@live.de>
2022-05-15 15:06:18 +0200

    adds info-page for the current challenege and a button that starts the challenge

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,13 +4,13 @@ function Navigation() {
     return (
         <nav className="bg-navigation bg-opacity-40 rounded-t-xs flex justify-around w-full h-12 p-3 items-center fixed bottom-0">
             <NavLink to="/">
-                <img src="../images/navigation-home-active.svg" />
+                <img src="../../images/navigation-home-active.svg" />
             </NavLink>
             <NavLink to="/browse">
-                <img src="../images/navigation-browse.svg" />
+                <img src="../../images/navigation-browse.svg" />
             </NavLink>
             <NavLink to="/profile">
-                <img src="../images/navigation-profile.svg" />
+                <img src="../../images/navigation-profile.svg" />
             </NavLink>
         </nav>
     );
