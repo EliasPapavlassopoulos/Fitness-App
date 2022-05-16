8057af3af004b4c81c53d100845a7d4bbc35d191
Elias Papavlassopoulos <e.papa@live.de>
2022-05-16 21:56:26 +0200

    removed comments on navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,17 +2,7 @@ import { NavLink } from "react-router-dom";
 import { ReactComponent as NavigationHomeComponent } from "../images/navigation-home.svg";
 import { ReactComponent as NavigationBrowseComponent } from "../images/navigation-browse.svg";
 import { ReactComponent as NavigationProfileComponent } from "../images/navigation-profile.svg";
-// Home is active on startup
-/*
-            <NavLink to="/" className={ }>
-                <img src="../../images/navigation-home-active.svg" />
-            </NavLink>
-            <NavLink to="/browse">
-                <img src="../../images/navigation-browse.svg" />
-            </NavLink>
-            <NavLink to="/profile">
-                <img src="../../images/navigation-profile.svg" />
-            </NavLink> */
+
 export default function Navigation() {
     return (
         <nav className="bg-navigation bg-opacity-40 rounded-t-xs flex justify-around w-full h-12 p-3 items-center fixed bottom-0">
