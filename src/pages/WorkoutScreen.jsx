e0d1633264045825d20838d014812c7ae7f4af96
Elias Papavlassopoulos <e.papa@live.de>
2022-06-15 17:33:02 +0200

    finished close confirmation window

--- src/pages/WorkoutScreen.jsx
+++ src/pages/WorkoutScreen.jsx
@@ -37,7 +37,7 @@ export default function WorkoutScreen() {
                 <button onClick={openCinfirmationWindow}><CloseButton className = "fixed right-[19px] top-[25px]" /></button>
                 {confirmationWindowActive && <CloseWindowConfirmWindow onClose={closeCinfirmationWindow} />}
                 <div className="duration-[150ms] z-10 h-[560px] w-full rounded-t-xs bg-backgroundLight fixed bottom-[-503px] left-0">
-                    <button className="mt-[15px] h-[28px] w-[28px] bg-background rounded-[50%] absolute right-[14px]" onClick={OpenInfoWindow}>
+                    <button className="mt-[15px] h-[28px] w-[28px] bg-background rounded-[50%] absolute right-[14px]">
                         <p className="text-center text-highlighted-textcolor relative left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] text-ft font-poppins font-bold leading-[30px]">i</p>
                     </button>
                 </div>
